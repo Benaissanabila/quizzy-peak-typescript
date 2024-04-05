42a486bea3c4a3bcc5f65432d1b2e99f152732d0
Benaissanabila <benaissa.nabila14@gmail.com>
2024-04-05 00:44:48 -0400

    test a jour

--- src/__tests__/Leaderboard.unit.spec.ts
+++ src/__tests__/Leaderboard.unit.spec.ts
@@ -1,3 +1,40 @@
+import Leaderboard from "../Leaderboard"; // Importez votre classe Leaderboard depuis votre fichier source
+
 describe('Leaderboard', () => {
-    
-})
+    let leaderboard: Leaderboard;
+
+    beforeEach(() => {
+        leaderboard = new Leaderboard();
+    });
+
+    describe('getUserScores', () => {
+        it('should return leaderboard scores for a specific user', () => {
+            leaderboard.addScore('category1', 'user1', 100);
+            leaderboard.addScore('category2', 'user1', 90);
+            expect(leaderboard.getUserScores('user1')).toEqual([
+                { username: 'user1', score: 100, category: 'category1' },
+                { username: 'user1', score: 90, category: 'category2' }
+            ]);
+        });
+
+        it('should return an empty array if user has no scores', () => {
+            expect(leaderboard.getUserScores('nonexistent_user')).toEqual([]);
+        });
+    });
+
+
+    /*describe('addScore', () => {
+        it('should return an empty array for new categories', () => {
+            expect(leaderboard).toEqual([]);
+        });
+        it('should add a new score for a user in a category if no previous score exists',()=>{
+            leaderboard.addScore('categoryA','userA',60)
+            expect(leaderboard).toEqual([{ category: 'categoryA', username:'userA', score: 60 }]);
+        });
+    })
+/*it('should find the user score if it exists in the category')
+        leaderboard.addScore('CategoryA', 'Alice', 90);
+        leaderboard.addScore('CategoryA', 'bob', 80));
+
+    });*/
+});
--- src/__tests__/Quiz.unit.spec.ts
+++ src/__tests__/Quiz.unit.spec.ts
@@ -1,5 +1,77 @@
-
+import UserProfile, { AccountType } from '../UserProfile';
+import Quiz from '../Quiz';
 
 describe('Quiz', () => {
-    
-})
+    let quiz: Quiz;
+    let adminUserProfile: UserProfile;
+    let userProfile: UserProfile;
+
+    beforeEach(() => {
+        quiz = new Quiz();
+        adminUserProfile = new UserProfile(
+            'David',
+            'David@gmail.com',
+            'David123',
+            AccountType.Admin,
+            'David John',
+            'Doe'
+        );
+
+        userProfile = new UserProfile(
+            'JaneS',
+            'jane@gmail.com',
+            'password456',
+            AccountType.User,
+            'Jane',
+            'Smith'
+        );
+
+    });
+
+    it('doit definir le profil dutilisateur actuel', () => {
+        adminUserProfile.authenticate('David@gmail.com', 'David123')
+        expect(adminUserProfile.isAuthenticated()).toBe(true);
+        quiz.setCurrentUser(adminUserProfile)
+        const currentUser = adminUserProfile
+
+        expect(currentUser).toEqual({
+            "_firstName": "David John",
+            "_lastName": "Doe",
+            "accountType": 0,
+            "authenticated": true,
+            "email": "David@gmail.com",
+            "password": "David123",
+            "username": "David"
+        });
+    });
+    it('doit definir le profil dutilisateur actuel', () => {
+        userProfile.authenticate('jane@gmail.com', 'password456')
+        expect(userProfile.isAuthenticated()).toBe(true);
+        quiz.setCurrentUser(userProfile)
+        const currentUser = userProfile
+
+        expect(currentUser).toEqual({
+            "_firstName": "Jane",
+            "_lastName": "Smith",
+            "accountType": 1,
+            "authenticated": true,
+            "email": "jane@gmail.com",
+            "password": "password456",
+            "username": "JaneS"
+        });
+    });
+    it('doit afficher message erreur sans autentification', () => {
+        expect(userProfile.isAuthenticated()).toBe(false);
+        expect(() => {
+            quiz.setCurrentUser(userProfile);
+        }).toThrowError('User must be authenticated to start a quiz.');
+    });
+    it('doit afficher message erreur sans autentification', () => {
+        expect(adminUserProfile.isAuthenticated()).toBe(false);
+        expect(() => {
+            quiz.setCurrentUser(adminUserProfile);
+        }).toThrowError('User must be authenticated to start a quiz.');
+    });
+});
+
+
